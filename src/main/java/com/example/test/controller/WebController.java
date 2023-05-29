bde124b2306b90a0196489ed1ea3036b57e13f5f
Alexander <bratenko6@gmail.com>
2023-05-29 14:14:23 +0300

    Test commit for merge

--- src/main/java/com/example/test/controller/WebController.java
+++ src/main/java/com/example/test/controller/WebController.java
@@ -1,2 +1,18 @@
-package com.example.test.controller;public class WebController {
+package com.example.test.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting.html";
+    }
+
 }
