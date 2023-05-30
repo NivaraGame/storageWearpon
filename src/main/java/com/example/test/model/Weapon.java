84465d020cc7acfa66c864dfa8215104c133cede
Alexander <bratenko6@gmail.com>
2023-05-30 15:07:46 +0300

    Add Jpa, DB, entity weapon

---
+++ src/main/java/com/example/test/model/Weapon.java
@@ -0,0 +1,34 @@
+package com.example.test.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Weapon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String rarity;
+
+    @Column(nullable = false)
+    private Integer damage;
+
+    @Column(nullable = false)
+    private Float weight;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+}
---
+++ src/main/java/com/example/test/repository/WeaponRepository.java
@@ -0,0 +1,10 @@
+package com.example.test.repository;
+
+import com.example.test.model.Weapon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WeaponRepository extends JpaRepository<Weapon, Long> {
+
+}
